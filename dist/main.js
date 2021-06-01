eb6fdc1c980d70ff0d3d785794e2422867cc6be5
michelleroos <hello.michelleroos@gmail.com>
2021-06-01 17:52:09 -0400

    second search

--- dist/main.js
+++ dist/main.js
@@ -1,30 +1,72 @@
 import "./main.css";
 const regeneratorRuntime = require("regenerator-runtime");
 const app_key = require('../config/keys').app_key;
-const searchForm = document.querySelector('form');
+const searchForm = document.querySelector('.searchForm');
+const filterForm = document.querySelector('.filterForm');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
 
-
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    searchQuery = e.target.querySelector('input').value;
-    fetchAPI(searchQuery);
+    searchQuery = e.target.querySelector('.searchQuery').value;
+    fetchSearchResults(searchQuery);
+    searchQuery = '';
 })
 
-async function fetchAPI(searchQuery) {
+async function fetchSearchResults(searchQuery) {
     const baseURL = 
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&includeIngredients=${searchQuery}&addRecipeInformation=true`
-        // type=main-course&
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&addRecipeInformation=true&includeIngredients=${searchQuery}`
     const response = await fetch(baseURL);
     const data = await response.json();
-    generateHTML(data.results)
+    generateHTML(data.results);
+    console.log(data);
+}
+
+filterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    searchQuery = urlParams.get('includeIngredients');
+    console.log(searchQuery);
+    let excludeQuery = e.target.querySelector('.excludeQuery').value;
+    let type = e.target.querySelector('.type').value;
+    fetchFilteredSearchResults(searchQuery, excludeQuery, type);
+    excludeQuery = '';
+})
+
+async function fetchFilteredSearchResults(searchQuery, excludeQuery, type) {
+    const baseURL =
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&addRecipeInformation=true&includeIngredients=${searchQuery}&excludeIngredients=${excludeQuery}&type=${type}`
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    generateHTML(data.results);
     console.log(data);
 }
 
 function generateHTML(results) {
-    let generatedHTML = '';
+    filterForm.innerHTML = `
+        <input class="excludeQuery" type="text" placeholder="Exclude comma separated ingredients">
+        <!-- <ion-icon name="search"></ion-icon> -->
+        <i class="fa fa-search" aria-hidden="true"></i>
+        <label for="type">Choose a type:</label>
+        <select name="type" class="type">
+            <option value="main%20course">Main course</option>
+            <option value="side%20dish">Side dish</option>
+            <option value="dessert">Dessert</option>
+            <option value="appetizer">Appetizer</option>
+            <option value="salad">Salad</option>
+            <option value="bread">Bread</option>
+            <option value="breakfast">Breakfast</option>
+            <option value="soup">Soup</option>
+            <option value="beverage">Beverage</option>
+            <option value="sauce">Sauce</option>
+            <option value="marinade">Marinade</option>
+            <option value="fingerood">Fingerfood</option>
+            <option value="snack">Snack</option>
+            <option value="drink">Drink</option>
+        </select>
+        <button type="submit">Filter</button>`;
+    let generatedHTML = ``;
     results.map(result => {
         generatedHTML += 
         `<div class="item">
@@ -33,7 +75,7 @@ function generateHTML(results) {
                 <h1 class="title">${result.title}</h1>
                 <a class="view-button" href=${result.sourceUrl}>View recipe</a>
             </div>
-            <p class="item-data">Calories: 120</p>
+            <p class="item-data">Vegetarian Vegan Gluten Free</p>
         </div>`
     })
     searchResultDiv.innerHTML = generatedHTML;
