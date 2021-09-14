7c7e9cc1364ff1896e340ecd592e14b21a9f094a
michelleroos <hello.michelleroos@gmail.com>
2021-09-13 22:45:43 -0400

    search results

--- src/main.js
+++ src/main.js
@@ -15,7 +15,8 @@ const regeneratorRuntime = require("regenerator-runtime");
 // const modal = document.querySelector('.modal');
 
 const searchInput = document.querySelector('#search-input');
-const searchResults = document.querySelector('#search-result-container');
+const searchResultsContainer = document.querySelector('#search-result-container');
+const searchResults = document.querySelector('#search-results');
 const scroopleSearch = document.querySelector('#scroople-search-btn');
 
 let searchQuery = '';
@@ -42,75 +43,22 @@ async function fetchSearchResults(searchQuery) {
     .catch(err => console.log(err))
 }
 
-// filterForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     let excludeQuery = e.target.querySelector('.excludeQuery').value;
-//     let type = e.target.querySelector('.type').value;
-//     let diet = [];
-//     for (const checkbox of e.target.querySelectorAll('input[name=diets]')) {
-//         if (checkbox.checked) {
-//             diet.push(checkbox.value);
-//         }
-//     }
-//     // searchQuery = 'cilantro' // TO BE REMOVED
-//     fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet);
-// })
-
-// // SECOND GET REQUEST
-// async function fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet) {
-//     axios({
-//         method: 'GET',
-//         url: `/recipes/${searchQuery}/filter`,
-//         params: {
-//             excludeQuery: excludeQuery,
-//             type: type,
-//             diet: diet
-//         }
-//     })
-//         .then(res => {
-//             console.log(res);
-//             generateHTML(res.data.results)
-//         })
-//         .catch(err => console.log(err))
-
-// }
-
-// function openModal() {
-//     viewRecipeModal.style.display = 'block';
-//     modalBackdrop.style.display = 'block';
-// }
-
-// function closeModal() {
-//     viewRecipeModal.style.display = 'none';
-//     modalBackdrop.style.display = 'none';
-// }
-
 function generateResults(results) {
 
-    let generatedResults = ``;
+    let generatedResults = `<p>About ${results.length} results (0.73 seconds)</p>`;
 
     results.forEach(result => {
-        const item = 
-        `<div class="item">
-            <img src=${result.image}>
-            <div class="flex-container">
-                <h1 class="title">${result.title}</h1>
-            </div>
-            <div class="inner-detail" style="display: none">
-                <h1>${result.title}</h1>
+        const resultItem = 
+        `<div class="result">
+            <p class="url">${result.sourceUrl}</p>
+            <a href=${result.sourceUrl} target="_blank"><h3 class="title">${result.title}</h3></a>
+            <p class="summary">${result.summary}</p>
+            <div class="result-links">
                 <p>${result.readyInMinutes} minutes</p>
                 <p>${result.servings} servings</p>
-                <img src=${result.image}>
-                <p id="summary">${result.summary} servings</p>
-                <a href=${result.sourceUrl} target="_blank">View recipe</a>
-                <form>
-                    <input type="email" id="email" placeholder="Enter your email">
-                    <button id="email-button">Email recipe</button>
-                </form>
-                <button id="calendar-button">Add to calendar</button>
             </div>
         </div>`
-        generatedResults += item;
+        generatedResults += resultItem;
     });
 
     searchResults.innerHTML = generatedResults;
