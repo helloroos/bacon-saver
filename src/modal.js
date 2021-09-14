86a685760bed97f357c69bd544e08a75eb64261e
michelleroos <hello.michelleroos@gmail.com>
2021-09-14 14:32:25 -0400

    close modal on click outside

--- src/modal.js
+++ src/modal.js
@@ -1,21 +1,3 @@
-// import "./main.css";
-
-// const regeneratorRuntime = require("regenerator-runtime");
-// const app_key = require('../config/keys').app_key;
-
-// // MODAL ELEMENTS IN HTML FILE
-// const viewRecipeModal = document.getElementById('view-recipe-modal');
-
-// const modalBackdrop = document.getElementById('modal-backdrop');
-
-// function openModal() {
-//     const viewButton = document.querySelector('.view-button');
-//     viewButton.addEventListener('click', () => {
-//         viewRecipeModal.style.display = 'block';
-//         modalBackdrop.style.display = 'block';
-//     })
-// }
-
 const howItWorks = document.querySelector('#how-scroople-works');
 const modalBg = document.querySelector('.modal-background');
 const times = document.querySelector('.fa-times');
@@ -26,4 +8,10 @@ export const modalClick = howItWorks.addEventListener('click', function () {
 
 export const closeModal = times.addEventListener('click', function () {
     modalBg.classList.remove('modal-active');
+})
+
+export const clickOutside = window.addEventListener('click', function (e) {
+    if (e.target == modalBg) {
+        modalBg.classList.remove('modal-active');
+    }
 })
