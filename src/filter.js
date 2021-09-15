2a296a84677c3e83af89112d4a3fb89eb2bc8fa0
michelleroos <hello.michelleroos@gmail.com>
2021-09-15 08:16:40 -0400

    inputs aligned

--- src/filter.js
+++ src/filter.js
@@ -1,29 +1,29 @@
-filterForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let excludeQuery = e.target.querySelector('.excludeQuery').value;
-  let type = e.target.querySelector('.type').value;
-  let diet = [];
-  for (const checkbox of e.target.querySelectorAll('input[name=diets]')) {
-    if (checkbox.checked) {
-      diet.push(checkbox.value);
-    }
-  }
-  // searchQuery = 'cilantro' // TO BE REMOVED
-  fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet);
-})
+// filterForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   let excludeQuery = e.target.querySelector('.excludeQuery').value;
+//   let type = e.target.querySelector('.type').value;
+//   let diet = [];
+//   for (const checkbox of e.target.querySelectorAll('input[name=diets]')) {
+//     if (checkbox.checked) {
+//       diet.push(checkbox.value);
+//     }
+//   }
+//   // searchQuery = 'cilantro' // TO BE REMOVED
+//   fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet);
+// })
 
-// SECOND GET REQUEST
-export async function fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet) {
-  axios({
-    method: 'GET',
-    url: `/recipes/${searchQuery}/filter`,
-    params: {
-      excludeQuery: excludeQuery,
-      type: type,
-      diet: diet
-    }
-  })
-    .then(res => {
-      generateHTML(res.data.results)
-    })
-    .catch(err => console.log(err))}
+// // SECOND GET REQUEST
+// export async function fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet) {
+//   axios({
+//     method: 'GET',
+//     url: `/recipes/${searchQuery}/filter`,
+//     params: {
+//       excludeQuery: excludeQuery,
+//       type: type,
+//       diet: diet
+//     }
+//   })
+//     .then(res => {
+//       generateHTML(res.data.results)
+//     })
+//     .catch(err => console.log(err))}
