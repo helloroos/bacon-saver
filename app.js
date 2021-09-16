bc2ce8f379604a959e5a11bef7b32ef006294e2a
michelleroos <hello.michelleroos@gmail.com>
2021-09-16 15:14:25 -0400

    consolidated js code

--- app.js
+++ app.js
@@ -13,49 +13,49 @@ const PORT = process.env.PORT || 8000; // process.env accesses heroku's environm
 app.use(express.static("dist"));
 
 app.get("/", (request, response) => {
-    // res.sendFile(path.join(__dirname, "./dist/index.html"));
-    response.sendFile(path.join(__dirname, "index.html"));
+  // res.sendFile(path.join(__dirname, "./dist/index.html"));
+  response.sendFile(path.join(__dirname, "index.html"));
 });
 
 // route to get recipes based on ingredient(s)
 app.get("/recipes/:searchQuery", (request, response) => {
-    // make api call using axios
-    const searchQuery = request.params.searchQuery;
-    axios({
-        method: 'GET',
-        url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&addRecipeInformation=true&includeIngredients=${searchQuery}`
+  // make api call using axios
+  const searchQuery = request.params.searchQuery;
+  axios({
+    method: 'GET',
+    url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&addRecipeInformation=true&includeIngredients=${searchQuery}`
+  })
+    .then((res) => {
+      response.send(res.data); // sends to frontend
     })
-        .then((res) => {
-            response.send(res.data); // sends to frontend
-        })
-        // .then((body) => {
-        //     let results = JSON.parse(body);
-        //     console.log(results); // logs to server
-        //     response.send(results); // sends to frontend
-        // });
+  // .then((body) => {
+  //     let results = JSON.parse(body);
+  //     console.log(results); // logs to server
+  //     response.send(results); // sends to frontend
+  // });
 });
 
 // route to get recipes based on filtered input
 app.get("/recipes/:query/filter", (request, response) => {
-    const searchQuery = request.params.query;
-    const excludeQuery = request.params.excludeQuery;
-    const type = request.params.type;
-    const diet = request.params.diet;
-    let baseURL =
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&addRecipeInformation=true&includeIngredients=${searchQuery}`;
-    if (excludeQuery) baseURL += `&excludeIngredients=${excludeQuery}`;
-    if (type) baseURL += `&type=${type}`;
-    if (diet) baseURL += `&diet=${diet}`;
-    axios({
-        method: 'GET',
-        url: baseURL
+  const searchQuery = request.params.query;
+  const excludeQuery = request.params.excludeQuery;
+  const type = request.params.type;
+  const diet = request.params.diet;
+  let baseURL =
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${app_key}&number=1000&addRecipeInformation=true&includeIngredients=${searchQuery}`;
+  if (excludeQuery) baseURL += `&excludeIngredients=${excludeQuery}`;
+  if (type) baseURL += `&type=${type}`;
+  if (diet) baseURL += `&diet=${diet}`;
+  axios({
+    method: 'GET',
+    url: baseURL
+  })
+    .then((res) => {
+      response.send(res.data);
     })
-        .then((res) => {
-            response.send(res.data);
-        })
 });
 
 app.listen(PORT, () => {
-    console.log(__dirname);
-    console.log(`listening on ${PORT}`);
+  console.log(__dirname);
+  console.log(`listening on ${PORT}`);
 });
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./styling/main.scss";
 import { modalClick, closeModal, clickOutside } from "./modal";
-import { fetchFilteredSearchResults } from "./filter";
+// import { fetchFilteredSearchResults } from "./filter";
 // import axios from "axios";
 const axios = require('axios').default;
 
@@ -16,38 +16,83 @@ const scroopleSearchBtn = document.querySelector('#scroople-search-btn');
 let searchQuery = '';
 
 mainSearchInput.addEventListener('keypress', (e) => {
-    // e.preventDefault();
-    if (e.key === 'Enter') {
-        searchQuery = mainSearchInput.value;
-        fetchSearchResults(searchQuery);
-    }
+  // e.preventDefault();
+  if (e.key === 'Enter') {
+    searchQuery = mainSearchInput.value;
+    fetchSearchResults(searchQuery);
+  }
 })
 
 scroopleSearchBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    searchQuery = mainSearchInput.value;
-    fetchSearchResults(searchQuery);
+  e.preventDefault();
+  searchQuery = mainSearchInput.value;
+  fetchSearchResults(searchQuery);
 })
 
 // FIRST GET REQUEST
 async function fetchSearchResults(searchQuery) {
-    axios({
-        method: 'GET',
-        url: `/recipes/${searchQuery}`
-    })
+  axios({
+    method: 'GET',
+    url: `/recipes/${searchQuery}`
+  })
     .then(res => {
-        generateResults(res.data.results)
+      generateResults(res.data.results)
     })
     .catch(err => console.log(err))
 }
 
+
+
+
+const filter = document.querySelector('#filter');
+let excludeInput = document.querySelector('#exclude-input');
+// let searchQuery = document.querySelector('#search-input').value;
+const mealType = document.querySelector('#meal-type');
+const diets = document.querySelectorAll('input[name=diets]');
+
+filter.addEventListener('click', () => {
+  // e.preventDefault();
+  let excludeQuery = excludeInput.value;
+  let type = mealType.value;
+  let diet = [];
+  for (const checkbox of diets) {
+    if (checkbox.checked) {
+      diet.push(checkbox.value);
+    }
+  }
+  console.log(excludeQuery);
+  console.log(type);
+  console.log(diet);
+  fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet);
+})
+
+// SECOND GET REQUEST
+export async function fetchFilteredSearchResults(searchQuery, excludeQuery, type, diet) {
+  axios({
+    method: 'GET',
+    url: `/recipes/${searchQuery}/filter`,
+    params: {
+      excludeQuery: excludeQuery,
+      type: type,
+      diet: diet
+    }
+  })
+  .then(res => {
+    generateResults(res.data.results)
+  })
+  .catch(err => console.log(err))
+}
+
+
+
+
 function generateResults(results) {
 
-    let generatedResults = `<p>About ${results.length} results (0.13 seconds)</p>`;
+  let generatedResults = `<p>About ${results.length} results (0.13 seconds)</p>`;
 
-    results.forEach(result => {
-        const resultItem = 
-        `<div class="result">
+  results.forEach(result => {
+    const resultItem =
+      `<div class="result">
             <p class="url">${result.sourceUrl}</p>
             <a href=${result.sourceUrl} target="_blank"><h3 class="title">${result.title}</h3></a>
             <p class="summary">${result.summary}</p>
@@ -56,14 +101,15 @@ function generateResults(results) {
                 <p>${result.servings} servings</p>
             </div>
         </div>`
-        generatedResults += resultItem;
-    });
+    generatedResults += resultItem;
+  });
 
-    // searchContainer.style.display = 'hidden';
-    searchContainer.classList.add('hidden');
+  // searchContainer.style.display = 'hidden';
+  searchContainer.classList.add('hidden');
 
-    searchResults.innerHTML = generatedResults;
-    // searchContainer.innerHTML = generatedResults;
+  searchResults.innerHTML = generatedResults;
+  // searchContainer.innerHTML = generatedResults;
 
-    searchInput.placeholder = searchQuery;
+  searchInput.value = searchQuery;
+  // searchInput.placeholder = searchQuery;
 }
