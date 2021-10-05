ccb25b67357beb06fbc52cee41062a8c0265f7e1
michelleroos <hello.michelleroos@gmail.com>
2021-10-05 19:27:11 -0400

    change webpach prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,19 @@
-// webpack.prod.js
+// const { merge } = require("webpack-merge");
+// const common = require("./webpack.common.js");
+
+// module.exports = merge(common, {
+//     mode: "production",
+//     devtool: "source-map"
+// });
+
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: "production",
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 });
